4eb44acb24246b3f0291535ead39a6b4e4ea4460
RockTharsh <52143660+RockTharsh@users.noreply.github.com>
2022-03-03 00:19:33 -0400

    Nav Bar animations has been added.

--- UI-Controller/UI-Conntroller.js
+++ UI-Controller/UI-Conntroller.js
@@ -1,10 +1,33 @@
+let menu = document.querySelector("#menu");
+let closeButton = document.getElementById("closeTap");
 
+
+
+//This class shows the menu if the class inactive is related 
 
 function showMenu(){
-
-    let main = document.querySelector("#menu");
-    main.classList.add("navActive");
-}
-function hideMenu(){
+    console.log("The menu has been showed")
+    if(menu.classList.contains("navInactive")){
+        menu.classList.remove("navInactive");
+    }
+    menu.classList.add("navActive");
+    setTimeout(()=>{
+        closeButton.style.display= "block";
+    }, 2000);
     
+}
+
+
+function hideMenu(){
+    menu.classList.remove("navActive");
+
+    menu.classList.add("navInactive");
+    setTimeout(()=>{
+        closeButton.style.display= "none";
+
+    }, 100
+  
+
+    );
+
 }
